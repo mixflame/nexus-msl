578650552060aa999e46e446d167869021719b9d
Jonathan Silverman <jsilver@mdx.la>
2012-06-18 00:12:00 -0700

    Welcome to the Nexus.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nexus::Application.config.secret_token = 'df8eb8cd8c7ef0540f221bb30f2c9ace4187ed8cc241fffed680049e8f38f21cea341605e4249dc12bd9b57ba442da3530e30c70f1600da5bf2aee6cd8119cc9'
