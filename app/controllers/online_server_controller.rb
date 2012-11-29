017e344a694fe59e72527b37ba33191ac0ac90b8
Jonathan "jsilver1er" Silverman <jsilverone@me.com>
2012-11-28 23:59:27 -0800

    ServerDropper, this is Nexus. we have an incoming server. can you please verify its integrity?

--- app/controllers/online_server_controller.rb
+++ app/controllers/online_server_controller.rb
@@ -3,18 +3,24 @@ class OnlineServerController < ApplicationController
 
   def online
     my_ip = request.ip
-    server = OnlineServer.set_online(params[:name], my_ip, params[:port], params[:host])
-    render :nothing => true
+    require 'net/http'
+    is_up = Net::HTTP.get_response(URI.parse("http://globalchat2.net/main/check_server?host=#{my_ip}&port=#{params[:port]}")).body == "200"
+    if is_up == true
+      OnlineServer.set_online(params[:name], my_ip, params[:port], params[:host])
+      render :text => "Listed on Nexus"
+    else
+      render :text => "Not listed", :status => 400
+    end
   end
 
   def offline
     my_ip = request.ip
-    server = OnlineServer.set_offline(my_ip)
+    OnlineServer.set_offline(my_ip)
     render :nothing => true
   end
 
   def msl
-  	render :text => OnlineServer.msl
+    render :text => OnlineServer.msl
   end
 
 end
