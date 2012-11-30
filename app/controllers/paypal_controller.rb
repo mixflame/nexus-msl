97d4c3a6981c2b4caf4095a732c9ef567d8906b3
Jonathan "jsilver1er" Silverman <jsilverone@me.com>
2012-11-30 03:00:19 -0800

    use ruby without knowing basic at your own peril

--- app/controllers/paypal_controller.rb
+++ app/controllers/paypal_controller.rb
@@ -44,12 +44,14 @@ class PaypalController < ApplicationController
     uri = URI.parse(paypal_url)
     # uri.scheme = "https"
 
-    http = Net::HTTP.start(uri.host, uri.port)
+    http = Net::HTTP.new(uri.host, uri.port)
     # http.open_timeout = 60
     # http.read_timeout = 60
     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
     http.use_ssl = true
     # response = http.post('/cgi-bin/webscr', query)
+
+    http.start
 
     response = http.post(uri.to_s, query)
 
