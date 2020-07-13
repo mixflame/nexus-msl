b0e8e09cc9a7f4a55909b51c135bc22446bbcd17
Jonathan Silverman <jonathansilverman@Jonathans-MBP-2.lan>
2020-07-12 18:32:23 -0600

    turn off connection requirement

--- app/controllers/online_server_controller.rb
+++ app/controllers/online_server_controller.rb
@@ -6,16 +6,16 @@ class OnlineServerController < ApplicationController
     # require 'net/http'
     # is_up = Net::HTTP.get_response(URI.parse("http://globalchat2.net/main/check_server?host=#{params[:host]}&port=#{params[:port]}")).body == "200"
     if params[:host] != 'localhost'
-      unless Rails.env.test?
-        require 'socket'
-        begin
-          s = TCPSocket.new ip, port
-          s.close
-        rescue
-          render :text => "Server is not online", :status => 404
-          return
-        end
-      end
+      # unless Rails.env.test?
+      #   require 'socket'
+      #   begin
+      #     s = TCPSocket.new ip, port
+      #     s.close
+      #   rescue
+      #     render :text => "Server is not online", :status => 404
+      #     return
+      #   end
+      # end
       
       OnlineServer.set_online(params[:name], my_ip, params[:port])
       render :text => "Listed on Nexus"
