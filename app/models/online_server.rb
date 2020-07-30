dbeee38f0693dfb7dd9eb25fe3fc7799e2bd4979
Jonathan Silverman <jsilverman2@gmail.com>
2020-07-30 16:21:45 -0600

    try port open code

--- app/models/online_server.rb
+++ app/models/online_server.rb
@@ -15,13 +15,29 @@ class OnlineServer < ActiveRecord::Base
     OnlineServer.destroy_all(["online_servers.ip = ?", ip])
   end
 
+  def self.port_open?(ip, port, seconds=1)
+    require 'socket'
+    require 'timeout'
+    # => checks if a port is open or not on a remote host
+    Timeout::timeout(seconds) do
+      begin
+        TCPSocket.new(ip, port).close
+        true
+      rescue Errno::ECONNREFUSED, Errno::EHOSTUNREACH, SocketError
+        false
+      end
+    end
+    rescue Timeout::Error
+      false
+  end
+
   def self.msl
     require 'socket'
     out = []
     OnlineServer.all.each { |s| 
 
           check = Net::Ping::External.new(host)
-          if !s.name.blank? && !s.ip.blank? && !s.port.blank? && check.ping?
+          if !s.name.blank? && !s.ip.blank? && !s.port.blank? && check.ping? && self.port_open?(s.ip, s.port)
             out << ["SERVER", s.name, s.ip, s.port ].join("::!!::") 
           end
 
